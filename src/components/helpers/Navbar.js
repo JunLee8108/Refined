577c903b0f57cb39ad3205c88732dffe99198d10
Jun Lee <jun@JunuiMacBookPro.lan>
2023-07-13 21:28:34 -0500

    mobile version added

--- src/components/helpers/Navbar.js
+++ src/components/helpers/Navbar.js
@@ -1,6 +1,22 @@
+import { clear } from "@testing-library/user-event/dist/clear";
 import "./Navbar.css";
+import { useState, useEffect } from "react";
 
 function Navbar() {
+  const [isHover, setHover] = useState(false);
+  const [navbarModal, setNavbarModal] = useState("");
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setNavbarModal("show-modal");
+    }, 100);
+
+    return () => {
+      clearTimeout(timer);
+      setNavbarModal("");
+    };
+  }, [isHover]);
+
   return (
     <div className="navbar-container">
       <nav>
@@ -10,7 +26,13 @@ function Navbar() {
             <h4>Seoul</h4>
           </div>
           <div className="brand-name-box-2">
-            <li>MEN</li>
+            <li
+              onClick={() => {
+                setHover(isHover => !isHover);
+              }}
+            >
+              MEN
+            </li>
             <li>WOMEN</li>
             <li>ACCESSORIES</li>
             <li>COLLECTIONS</li>
@@ -22,6 +44,25 @@ function Navbar() {
           <li>BAG</li>
         </ul>
       </nav>
+
+      {isHover ? (
+        <nav className={"navbar-modal " + navbarModal}>
+          <ul className="brand-name display-flex-start">
+            <div className="brand-name-box-1 flex-column">
+              <div>
+                <li>‣ New Arrival</li>
+                <li>‣ Coats & Jackets</li>
+                <li>‣ Shirts</li>
+                <li>‣ Kintwear</li>
+                <li>‣ Jeans</li>
+                <li>‣ Pants</li>
+              </div>
+            </div>
+            <div className="brand-name-box-2"></div>
+          </ul>
+          <ul className="nav-menu display-flex-start"></ul>
+        </nav>
+      ) : null}
     </div>
   );
 }
